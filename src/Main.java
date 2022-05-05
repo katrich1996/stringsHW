cfa1c803d6603e2586586d7c89bbb21151f39d61
katrich1996 <sergey.katrich96@mail.ru>
2022-05-05 21:06:15 +0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -14,22 +14,6 @@ class HW {
         System.out.println("”Данные ФИО сотрудника для заполнения отчета — " + strUpper);
     }
 }
-///task1-3
-class HW {
-    public static void main(String[] args) {
-        String firstName = "Ivan ";
-        String middleName = "Ivanovich ";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleName;
-        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
-        String strUpper = fullName.toUpperCase(Locale.ROOT);
-        fullName = fullName.replace(" ", "; ");
-        System.out.println("Данные ФИО сотрудника для административного отдела — " + fullName);
-        System.out.println("”Данные ФИО сотрудника для заполнения отчета — " + strUpper);
-    }
-}
-
-
 ///task4
 class dsa {
     public static void main(String[] args) {
