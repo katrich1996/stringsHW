90d055f53ffda13a53b5218a2af0e582ef75c597
katrich1996 <sergey.katrich96@mail.ru>
2022-05-05 21:04:31 +0300

    Test

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
-public class Main {
+import java.util.Locale;
+
+///task1-3
+class HW {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+        String strUpper = fullName.toUpperCase(Locale.ROOT);
+        fullName = fullName.replace(" ", "; ");
+        System.out.println("Данные ФИО сотрудника для административного отдела — " + fullName);
+        System.out.println("”Данные ФИО сотрудника для заполнения отчета — " + strUpper);
     }
 }
